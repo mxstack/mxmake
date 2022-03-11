56d45c261718d2006a9d954fa17aa67aaae96cab
Robert Niederreiter <rnix@squarewave.at>
2022-03-11 10:57:26 +0100

    Improve python-ldap target

--- files/cfg/python-ldap.mk
+++ files/cfg/python-ldap.mk
@@ -2,12 +2,26 @@
 # python-ldap
 ###############################################################################
 
+PYTHON_LDAP_SENTINEL:=$(SENTINEL_FOLDER)/python-ldap.sentinel
+
 .PHONY: python-ldap
-python-ldap: openldap venv
+python-ldap: $(PYTHON_LDAP_SENTINEL)
+
+$(PYTHON_LDAP_SENTINEL): $(VENV_SENTINEL) $(OPENLDAP_SENTINEL)
 	@$(PIP_BIN) install \
+		--force-reinstall \
 		--no-use-pep517 \
 		--global-option=build_ext \
 		--global-option="-I$(OPENLDAP_DIR)/include" \
 		--global-option="-L$(OPENLDAP_DIR)/lib" \
 		--global-option="-R$(OPENLDAP_DIR)/lib" \
 		python-ldap
+	@touch $(PYTHON_LDAP_SENTINEL)
+
+.PHONY: python-ldap-dirty
+python-ldap-dirty:
+	@rm -f $(PYTHON_LDAP_SENTINEL)
+
+.PHONY: python-ldap-clean
+python-ldap-clean: python-ldap-dirty
+	@test -e $(PIP_BIN) && $(PIP_BIN) uninstall -y python-ldap
