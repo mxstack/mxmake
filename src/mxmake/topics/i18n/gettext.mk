5f04ab16103743ea8f9ad1df072ca245e3ad33cb
Robert Niederreiter <rnix@squarewave.at>
2023-03-03 16:33:42 +0100

    gettext-create target works

--- src/mxmake/topics/i18n/gettext.mk
+++ src/mxmake/topics/i18n/gettext.mk
@@ -32,15 +32,19 @@ SYSTEM_DEPENDENCIES+=gettext
 
 .PHONY: gettext-create
 gettext-create:
-	@echo "Create message catalogs"
-	@for lang in "$(GETTEXT_LANGUAGES); do \
-		if [ ! -e "$(LOCALES_PATH)/$(lang)/LC_MESSAGES/$(DOMAIN).po" ]; then \
-			mkdir -p $(LOCALES_PATH)/$(lang)/LC_MESSAGES; \
+	@if [ ! -e "$(GETTEXT_LOCALES_PATH)/$(GETTEXT_DOMAIN).pot" ]; then \
+		echo "Create pot file"; \
+		mkdir -p $(GETTEXT_LOCALES_PATH); \
+		touch $(GETTEXT_LOCALES_PATH)/$(GETTEXT_DOMAIN).pot; \
+	fi
+	@for lang in $(GETTEXT_LANGUAGES); do \
+		if [ ! -e "$(GETTEXT_LOCALES_PATH)/$$lang/LC_MESSAGES/$(GETTEXT_DOMAIN).po" ]; then \
+			mkdir -p $(GETTEXT_LOCALES_PATH)/$$lang/LC_MESSAGES; \
 			msginit \
-				-i $(LOCALES_PATH)/$(DOMAIN).pot \
-				-o $(LOCALES_PATH)/$(lang)/LC_MESSAGES/$(DOMAIN).po \
-				-l $(lang); \
-		fi; \
+				-i $(GETTEXT_LOCALES_PATH)/$(GETTEXT_DOMAIN).pot \
+				-o $(GETTEXT_LOCALES_PATH)/$$lang/LC_MESSAGES/$(GETTEXT_DOMAIN).po \
+				-l $$lang; \
+		fi \
 	done
 
 #.PHONY: gettext-update
@@ -48,8 +52,8 @@ gettext-create:
 #	@echo "Update translations"
 
 #echo "Update translations"
-#for po in "$LOCALES_PATH"/*/LC_MESSAGES/$DOMAIN.po; do
-#    msgmerge -o "$po" "$po" "$LOCALES_PATH"/$DOMAIN.pot
+#for po in "$GETTEXT_LOCALES_PATH"/*/LC_MESSAGES/$GETTEXT_DOMAIN.po; do
+#    msgmerge -o "$po" "$po" "$GETTEXT_LOCALES_PATH"/$GETTEXT_DOMAIN.pot
 #done
 
 #.PHONY: gettext-compile
@@ -57,8 +61,8 @@ gettext-create:
 #	@echo "Compile message catalogs"
 
 #echo "Compile message catalogs"
-#for po in "$LOCALES_PATH"/*/LC_MESSAGES/*.po; do
-#	lg=${po##$LOCALES_PATH/}
+#for po in "$GETTEXT_LOCALES_PATH"/*/LC_MESSAGES/*.po; do
+#	lg=${po##$GETTEXT_LOCALES_PATH/}
 #	lg=${lg%%/LC_MESSAGES/*}
 #	echo -n "$lg: "
 #	msgfmt --statistics -o "${po%.*}.mo" "$po"
