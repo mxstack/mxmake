2824f05c7ad6bfa372d5acb41040b41a8f56a7e9
Robert Niederreiter <rnix@squarewave.at>
2024-02-06 12:05:15 +0100

    Add `wtr` (Web test runner) domain to `js` topic.

---
+++ src/mxmake/topics/js/wtr.mk
@@ -0,0 +1,28 @@
+#:[wtr]
+#:title = Web test runner
+#:description = Run JavaScript tests using web test runner.
+#:depends = js.npm
+#:
+#:[target.wtr]
+#:description = Execute web test runner.
+#:
+#:[setting.WTR_CONFIG]
+#:description = Web test runner config file.
+#:default = wtr.config.mjs
+#:
+#:[setting.WTR_OPTIONS]
+#:description = Web test runner additional command line options.
+#:default = --coverage
+
+##############################################################################
+# web test runner
+##############################################################################
+
+# extend npm dev packages
+NPM_DEV_PACKAGES+=\
+	@web/test-runner \
+	@web/dev-server-import-maps
+
+.PHONY: wtr
+wtr: $(NPM_TARGET)
+	@$(NPM_PREFIX)/node_modules/.bin/web-test-runner $(WTR_OPTIONS) --config $(WTR_CONFIG)
